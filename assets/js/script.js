88d892756f929017d584157e473dd524686abe03
ctr-code <codeinstitute@charlesreilly.com>
2026-07-22 09:47:49 +0100

    Add javascript; game basically works

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+var currentGame;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".btn-big");
+    for (const button of buttons) {
+        button.addEventListener("click", function () {
+            const gameType = this.dataset.type;
+            initialiseGame(gameType);
+        });
+    }
+
+    document.getElementById("submit").addEventListener("click", checkAnswer);
+
+    initialiseGame("add");
+});
+
+function initialiseGame(gameType) {
+    const game = makeGame(gameType);
+
+    document.getElementById("operand1").innerText = game.op1;
+    document.getElementById("operand2").innerText = game.op2;
+    document.getElementById("operator").innerHTML = game.op;
+    document.getElementById("answer-box").value = "";
+
+    currentGame = game;
+}
+
+function makeGame(gameType) {
+    function make(op1, op2, r, op) {
+        return { type: gameType, op1: op1, op2: op2, result: r, op: op };
+    }
+
+    const num1 = Math.floor(Math.random() * 25) + 1;
+    const num2 = Math.floor(Math.random() * 25) + 1;
+
+    switch (gameType) {
+        case "add":
+            return make(num1, num2, num1 + num2, "+");
+        case "subtract":
+            return make(num1, num2, num1 - num2, "-");
+        case "multiply":
+            return make(num1, num2, num1 * num2, "&times;");
+        case "divide":
+            return make(num1 * num2, num2, num1, "&divide;");
+    }
+}
+
+function checkAnswer() {
+    const answer = document.getElementById("answer-box").value;
+    if (parseInt(answer) === currentGame.result) {
+        document.getElementById("correct").innerText++;
+    } else {
+        document.getElementById("incorrect").innerText++;
+    }
+    initialiseGame(currentGame.type);
+}
